ac61a1a454bc8e52a905a62a8936301c3a4a31ac
myamamoto88 <myamamoto@aiming-inc.com>
2017-03-26 11:49:34 +0900

    Implement SnowFlake::Generator

--- lib/snow_flake/generator.rb
+++ lib/snow_flake/generator.rb
@@ -1,12 +1,14 @@
 class SnowFlake
   class Generator
+    class TimeMoveBackWard < StandardError; end
+
     def id
       timestamp_ms = current_timestmap_ms
 
       commander.process(timestamp_ms)
       commander.conflate
     ensure
-      wait_for_next_time(timestamp_ms) if commander.wait?
+      timestamp_ms = fetch_next_time(timestamp_ms) if commander.wait?
       @last_timestamp_ms = timestamp_ms
     end
 
@@ -25,9 +27,20 @@ class SnowFlake
     end
 
     def current_timestmap_ms
+      timestamp_ms = (Time.now.to_f * 1_000).to_i
+      diff = timestamp_ms - last_timestamp_ms
+      if diff.negative?
+        raise TimeMoveBackWard,
+          "Refusing to generate id, because time is back (#{diff} ms)"
+      end
+      timestamp_ms
     end
 
-    def wait_for_next_time(timestamp_ms)
+    def fetch_next_time(timestamp_ms)
+      while timestamp_ms <= last_timestamp_ms
+        timestamp_ms = current_timestamp_ms
+      end
+      timestamp_ms
     end
   end
 end
